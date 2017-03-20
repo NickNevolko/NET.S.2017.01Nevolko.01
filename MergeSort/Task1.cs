a775a65dce23e72a08825ab211112c1c4d8ae2b2
nicknevolko <nikolay.nevolko@gmail.com>
2017-03-20 14:21:42 +0300

    added tests

---
+++ MergeSort/Task1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    /// <summary>
+    /// static class which contains a MergeSort method
+    /// </summary>
+    public class Task1
+    {
+        /// <summary>
+        /// Method which sorts an array,
+        /// has a one parameter
+        /// </summary>
+        /// <param name="array">one parameter is array of ints. </param>
+        /// <returns>returns a new sorted array of integers</returns>
+        public static void Sorting(int[] array)
+        {
+            int [] sorted_array = MergeSort(array);
+            Array.Copy(sorted_array, array, array.Length);
+        }
+
+        /// <summary>
+        /// Split and calls a Merge method
+        /// </summary>
+        private static int[] MergeSort(int[] arr)
+        {
+            if (arr == null)
+                throw new ArgumentNullException(nameof(arr));
+
+            if (arr.Length == 0 || arr.Length == 1)
+                return arr;
+
+            int middle = arr.Length / 2;
+
+            return Merge(MergeSort(arr.Take(middle).ToArray()), MergeSort(arr.Skip(middle).ToArray()));
+        }
+
+        /// <summary>
+        /// Merges arrays to new one result array
+        /// </summary>
+        private static int[] Merge(int[] array1, int[] array2)
+        {
+            int a = 0;
+            int b = 0;
+            int[] merged = new int[array1.Length + array2.Length];
+
+            for (int i = 0; i < merged.Length; i++)
+            {
+                if (a < array1.Length && b < array2.Length)
+                    if (array1[a] > array2[b])
+                    {
+                        merged[i] = array2[b++];
+                    }
+                    else
+                    {
+                        merged[i] = array1[a++];
+                    }
+                else if (b < array2.Length)
+                {
+
+                    merged[i] = array2[b++];
+                }
+                else
+                    merged[i] = array1[a++];
+            }
+            return merged;
+        }
+    }
+}
+
---
+++ NUnit.Tests1/TestClass.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using static Task.Task1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnit.Tests1
+{
+    /// <summary>
+    /// Test class checks results of MergeSort
+    /// </summary>
+    [TestFixture]
+    public class TestClass
+    {
+        /// <summary>
+        /// void method with no parameters
+        /// </summary>
+        [Test]
+        public void Sorting_SortArray()
+        {
+            int[] test_arr1 = { 3, 1, 56, 87, 0, 19, 54 };
+            int[] test_arr2 = { 3, 1, 56, 87, 0, 19, 54 };
+            Array.Sort(test_arr1);
+            Sorting(test_arr2);
+            CollectionAssert.AreEqual(test_arr1, test_arr2);
+        }
+
+        [TestCase(null)]
+        public void Sorting_ThrowsArgumentNullException(int[] array)
+        {
+            Assert.Throws<ArgumentNullException>(() => Sorting(array));
+        }
+    }
+}
